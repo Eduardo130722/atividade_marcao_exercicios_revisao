cc0955b73c1c2e8366b11b8b818d346bdfb5edaf
Eduardo130722 <163954027+Eduardo130722@users.noreply.github.com>
2024-09-04 13:43:37 -0300

    ma vontade paizin

--- 4 idade.c
+++ 4 idade.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-int main(){
-    int num;
-
-    printf("Digite sua idade: ");
-    scanf("%d", &num);
-
-    if(num>=18){
-        printf("Voce eh maior de idade!!");
-    }
-    else
-    printf("Voce ainda nao eh maior de idade!!");
-}
